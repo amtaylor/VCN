496a624d3d024540db6cbefd7967488c8e939414
Amit Taylor <amtaylor7@gmail.com>
2014-02-19 12:26:53 -0800

    edits for Rahul to check out v2

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,3 +1,10 @@
 class Investor < ActiveRecord::Base
   belongs_to :company
+
+	class << self
+	 def company_for_investor(investor)
+	   Investor.where(:name => investor.name).uniq.map(&:companies).map(&:name).order("name ASC")
+	 end
+	end
+
 end
