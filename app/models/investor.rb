f8a88999e2965841924658f4bf4bb45fd07ad666
Amit Taylor <amtaylor7@gmail.com>
2014-02-19 23:58:17 -0800

    updated company format

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -3,8 +3,8 @@ class Investor < ActiveRecord::Base
 
 	class << self
 	 def company_for_investor(investor)
-     Investor.where(:name => investor.name).map(&:company).map(&:name).uniq #This gives back array of strings of company names
-	   #Investor.where(:name => investor.name).map(&:company).uniq #This gives back array of objects of companies
+     	#Investor.where(:name => investor.name).map(&:company).map(&:name).uniq #This gives back array of strings of company names
+	 	Investor.where(:name => investor.name).map(&:company).uniq #This gives back array of objects of companies
 	 end
 	end
 
