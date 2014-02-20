641c7e0a00cb4e817dba43b3355150f7adb8294d
Rahul <rahulpattamatta@gmail.com>
2014-02-19 20:35:06 -0800

    query for intersecting companies

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,3 +1,10 @@
 class Investor < ActiveRecord::Base
   belongs_to :company
+
+  class << self
+  	def company_for_investor(investor)
+  	  Investor.where(:name => investor.name).map(&:company).map(&:name).uniq
+    end
+  end
+
 end
