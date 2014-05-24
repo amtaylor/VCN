4de17479bccd607142a27ef824c000448bc41781
Rahul <rpattamatta@kabam.com>
2014-05-23 17:35:34 -0700

    ignore mailer and deploy

--- app/mailers/new_investment_mailer.rb
+++ app/mailers/new_investment_mailer.rb
@@ -1,10 +1,10 @@
 class NewInvestmentMailer < ActionMailer::Base
-  default to: #List of people whose companies got funded
-  default from: #'no-reply@vcn.com'
+  #default to: #List of people whose companies got funded
+  #default from: #'no-reply@vcn.com'
 
-  def new_investment_email(user)
-    @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'The company you are following just got funded!')
-  end  
+  #def new_investment_email(user)
+  #  @user = user
+  #  @url  = 'http://example.com/login'
+  #  mail(to: @user.email, subject: 'The company you are following just got funded!')
+  #end  
 end
