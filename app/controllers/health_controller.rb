126a6dae425c046683f0ac0dad0302f615938a5d
Rahul <rahulpattamatta@gmail.com>
2014-06-05 22:33:25 -0700

    droidddd

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -2,6 +2,6 @@ class HealthController < ApplicationController
 	skip_before_filter :user
 
 	def index
-	  render :json => {status: 200}
+	  render :json => {status: 200, description: "Who do you think I am? R2D2?"}
 	end
 end
