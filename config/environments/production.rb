3833051058dc4e498acbaf5ba386cdf7a4c63286
Rahul <rahulpattamatta@gmail.com>
2014-08-23 23:50:06 -0700

    Memcached config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,15 @@ Vcn::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2
+                    }
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
